cd4775b459661f07bf8a169412fbcc29c5bd346d
sanjai2614 <sanjai63698@gmail.com>
2026-09-08 03:48:05 +0530

    update cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import github from '../assets/github.png'
 import gmail from '../assets/gmail.png'
 import hero from '../assets/hero.png'
 import hi from '../assets/hi.png'
-import CV from '../assets/Sanjai Mern Stack.pdf'
+import CV from '../assets/Sanjai-resume.pdf'
 
 const Hero = ({ darkMode }) => {
 
